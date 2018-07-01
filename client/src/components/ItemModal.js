400a59a6025ea646313fc14d8e2b6f39f74a7220
Joseph Davis <joseph@josephndavis.com>
2018-07-01 16:47:57 -0500

    Connect to backend

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -10,7 +10,6 @@ import {
   Label,
   Input
 } from 'reactstrap';
-import uuid from 'uuid';
 
 import { addItem } from '../actions/itemActions';
 
@@ -32,7 +31,6 @@ class ItemModal extends Component {
     e.preventDefault();
 
     const newItem = {
-      id: uuid(),
       name: this.state.name
     };
 
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { getItems, deleteItem } from '../actions/itemActions';
@@ -21,14 +21,14 @@ class ShoppingList extends Component {
     return (
       <ListGroup>
         <TransitionGroup className="shopping-list">
-          {items.map(({ id, name }) => (
-            <CSSTransition classNames="fade" key={id} timeout={500}>
+          {items.map(({ _id, name }) => (
+            <CSSTransition classNames="fade" key={_id} timeout={500}>
               <ListGroupItem>
                 <Button
                   className="remove-btn"
                   color="danger"
                   size="sm"
-                  onClick={this.onDeleteItem.bind(this, id)}>
+                  onClick={this.onDeleteItem.bind(this, _id)}>
                   &times;
                 </Button>
                 {name}
