60ecef24851aec2b3312309b3491b02bb34c84d7
Joseph Davis <joseph.n.davis.98@gmail.com>
2021-10-09 10:50:01 -0500

    Update mongoose options

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
