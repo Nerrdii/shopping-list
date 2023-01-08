13f13704f7a9f17d4fc24538a3ef38d67d853261
Joseph Davis <joseph.n.davis.98@gmail.com>
2023-01-08 10:04:02 -0600

    Update react-redux & install redux toolkit

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 
-import rootReducer from './reducers';
+import authReducer from './reducers/authReducer';
+import errorReducer from './reducers/errorReducer';
+import itemReducer from './reducers/itemReducer';
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    error: errorReducer,
+    item: itemReducer,
+  },
+});
 
 export default store;
